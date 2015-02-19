79d22ea7f3ba548a3ba0eb8ae0f3d3a154b759ec
Chad Reed <chadreed@lawn-128-61-29-85.lawn.gatech.edu>
2015-02-19 10:01:35 -0500

    updated dbconnectfile

--- DBConnect.js
+++ DBConnect.js
@@ -1,10 +1,7 @@
-try {
-var shortName = 'DB/ATPDB.db';
-var version = '3.0';
-var displayName = 'Display Information';
-var maxSize = 655360; // in bytes
-db = openDatabase(shortName, version, displayName, maxSize);
-}
-catch(e) {
-console.log(e);
-}
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('DB/ATPDB.db'); 
+db.serialize(function() {
+db.run("SELECT COUNT(*) FROM ATPDATA");
+db.each("SELECT COUNT(*) as test FROM ATPDATA", function(err, row) { console.log(row.test) } );
+});
+db.close();
