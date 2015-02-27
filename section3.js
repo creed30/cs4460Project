37fd439f662a1485cb6d3ec660b3ac9d49f41a3d
Chad Reed <chadreed@Chads-MacBook-Pro.local>
2015-02-26 22:38:04 -0500

    Added first version of graph

--- section3.js
+++ section3.js
@@ -1,3 +1,4 @@
+function go() {
 var width = window.innerWidth
 	|| document.documentElement.clientWidth
 	|| document.body.clientWidth;;
@@ -8,29 +9,108 @@ var height = window.innerHeight
 
 var hL = height / 5 ;
 
+var padding = 10;
+
 var chart = d3.select(".chart")
     .attr("width", width)
     .attr("height", height);
 
-var x = document.getElementById("section3");
-x.innerHTML = "Browser inner window width: " + width + ", height: " + height + ".";
-
-
+// var x = document.getElementById("section3");
+// x.innerHTML = "Browser inner window width: " + width + ", height: " + height + ".";
 
 window.onresize = resize;
+var m = [80, 80, 80, 80]; 
+	var w = width - m[1] - m[3]; 
+	var h = height - m[0] - m[2]; 
+	
+	var data= document.getElementById('demoWRank').innerHTML.split(",");
+	console.log(data);
+	function formatCurrency (d){
+  	return "$" + d; 
+	}
 
-var svgContainer = d3.select("body").append("svg")
-                                    .attr("width", width)
-                                    .attr("height", height);
+	var xLabels = d3.time.scale().domain([new Date(2013, 0, 1), new Date(2013, 11, 31)]).range([0, w]);
+	var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+	var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
 
-//Draw the line
-var xAxis = svgContainer.append("line")
-                         .attr("x1", 100)
-                         .attr("y1", hL)
-                         .attr("x2", width - 100)
-                         .attr("y2", hL)
-                         .attr("stroke-width", 5)
-                         .attr("stroke", "black");
+	var line = d3.svg.line()
+		.x(function(d,i) { 
+			console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+			return x(i); 
+		})
+		.y(function(d) { 
+			console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+			return y(d); 
+		})
+
+	var graph = d3.select("#section3").append("svg:svg")
+	      .attr("width", w + m[1] + m[3])
+	      .attr("height", h + m[0] + m[2])
+	    .append("svg:g")
+	      .attr("transform", "translate(" + 120 + "," + m[0] + ")");
+
+	var xAxis = d3.svg.axis().scale(xLabels).ticks(d3.time.months).tickFormat(d3.time.format("%B")).tickSize(-h).tickSubdivide(true);
+	graph.append("svg:g")
+	      .attr("class", "x axis")
+	      .attr("transform", "translate(0," + h + ")")
+	      .call(xAxis);
+
+	var yAxisLeft = d3.svg.axis().scale(y).ticks(7).tickFormat(formatCurrency).orient("left");
+	graph.append("svg:g")
+	      .attr("class", "y axis")
+	      .attr("transform", "translate(-25,0)")
+	      .call(yAxisLeft);
+
+		graph.append("svg:path")
+			.attr("d", line(data))
+			.attr('class', 'line');
+		}
+/*
+var w = width,
+    h = height/3,
+    pad = 20,
+    left_pad = 150,
+    Data_url = '/data.json';
+ 
+var svg = d3.select("#section3")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+ 
+var x = d3.scale.linear().domain([0, 11]).range([left_pad, w-pad]),
+    y = d3.scale.linear().domain([0,11]).range([pad, h-pad*2]);
+	
+var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+    yAxis = d3.svg.axis().scale(y).orient("left");
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0, "+(h-pad)+")")
+    .call(xAxis);
+ 
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate("+(left_pad-pad)+", 0)")
+    .call(yAxis);
+	
+*/	
+//Create the SVG Viewport
+// var svgContainer = d3.select("body").append("svg")
+//                                      .attr("width", width)
+//                                      .attr("height", height);
+//
+// //Create the Scale we will use for the Axis
+// var axisScale = d3.scale.linear()
+//                          .domain([0,100])
+//                          .range([padding, width-padding*2]);
+//
+// //Create the Axis
+// var xAxis = d3.svg.axis()
+//                    .scale(axisScale);
+//
+//
+// //Create an SVG group Element for the Axis elements and call the xAxis function
+// var xAxisGroup = svgContainer.append("g")
+//                               .call(xAxis);
 
 function resize()
 {
