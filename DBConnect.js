e1c23fc3763434fdc56a60909f28fb303033252a
SpencerMalone <malone.spencer@gmail.com>
2015-03-04 19:51:22 -0500

    Removed unnecessary async calls.

--- DBConnect.js
+++ DBConnect.js
@@ -17,59 +17,35 @@ xhr.send();
 
 
 function selectCount(column) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', '/DB/ATPDB.db', true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function(e) {
 	  var contents = db.exec("SELECT COUNT(*) FROM ATPDATA");
 	  console.log(contents);
 	  var test = contents[0].values[0][0];
 	  	// Set the document text to the return value
 		document.getElementById('demo').innerHTML = test;
-	}
-	xhr.send();
 }
 
 function selectWRank(player,date) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', '/DB/ATPDB.db', true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function(e) {
 	  var contents = db.exec("SELECT DISTINCT Wrank,date FROM ATPDATA WHERE Winner='"+player+"' AND Date LIKE '%%%%%%"+date+"'");
 	  console.log(contents);
 	  var test = contents[0].values;
 	  	// Set the document text to the return value
 		document.getElementById('demoWRank').innerHTML = test;
-	}
-	xhr.send();
 }
 
 function selectLRank(player) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', '/DB/ATPDB.db', true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function(e) {
 	  var contents = db.exec("SELECT Lrank FROM ATPDATA WHERE Loser='"+player+"'");
 	  console.log(contents);
 	  var test = contents[0].values;
 	  	// Set the document text to the return value
 		document.getElementById('demoLRank').innerHTML = test;
-	}
-	xhr.send();
 }
 
 function selectWLRank(player) {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', '/DB/ATPDB.db', true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function(e) {
 	  var contents = db.exec("SELECT Lrank FROM ATPDATA WHERE Loser='"+player+"' UNION ALL SELECT Wrank FROM ATPDATA WHERE Winner='"+player+"'");
 	  console.log(contents);
 	  var test = contents[0].values;
 	  	// Set the document text to the return value
 		document.getElementById('demoWLRank').innerHTML = test;
-	}
-	xhr.send();
 }
 
 // function selectPlayers(year) {
@@ -89,17 +65,10 @@ function selectWLRank(player) {
 //   // fillPlayer();
 // }
 function selectPlayers() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', '/DB/ATPDB.db', true);
-	xhr.responseType = 'arraybuffer';
-	xhr.onload = function(e) {
 	  var contents = db.exec("SELECT Loser FROM ATPDATA UNION SELECT Winner FROM ATPDATA");
 	  console.log(contents);
 	  var test = contents[0].values;
 	  	// Set the document text to the return value
 		// alert(typeOf test);
     document.getElementById('players').innerHTML = test;
-	}
-	xhr.send();
-  // fillPlayer();
 }
